3158385d45d750737d4c1506b19c468b5a795cac
coteruto <joseigarp@gmail.com>
2017-11-16 17:00:47 -0400

    pequeño cambio

--- httpd-2.4.28-o102l-x86-vc14/Apache24/htdocs/seleccion_primeraEtapa/plantillas/formulario_inicial.inc.php
+++ httpd-2.4.28-o102l-x86-vc14/Apache24/htdocs/seleccion_primeraEtapa/plantillas/formulario_inicial.inc.php
@@ -1,41 +1,41 @@
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 1</label>
-        <input type="date" class="form-control" name="fecha1">
+        <input type="date" class="form-control" name="fecha1" required>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero1">
+        <input type="number" class="form-control" name="numero1" required>
     </div>
 </div>
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 2</label>
-        <input type="date" class="form-control" name="fecha2">
+        <input type="date" class="form-control" name="fecha2" required>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero2">
+        <input type="number" class="form-control" name="numero2" required>
     </div>
 </div>
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 3</label>
-        <input type="date" class="form-control" name="fecha3">
+        <input type="date" class="form-control" name="fecha3" required>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero3">
+        <input type="number" class="form-control" name="numero3" required>
     </div>
 </div>
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 4</label>
-        <input type="date" class="form-control" name="fecha4">
+        <input type="date" class="form-control" name="fecha4" required>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero4">
+        <input type="number" class="form-control" name="numero4" required>
     </div>
 </div>
 <button type="submit" class="btn btn-block btn-primary" name="submit">Calcular</button>
--- httpd-2.4.28-o102l-x86-vc14/Apache24/htdocs/seleccion_primeraEtapa/plantillas/formulario_validado.inc.php
+++ httpd-2.4.28-o102l-x86-vc14/Apache24/htdocs/seleccion_primeraEtapa/plantillas/formulario_validado.inc.php
@@ -1,48 +1,48 @@
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 1</label>
-        <input type="date" class="form-control" name="fecha1" <?php $validador->mostrarFecha1(); ?>>
+        <input type="date" class="form-control" name="fecha1" <?php $validador->mostrarFecha1(); ?> required>
         <?php $validador->mostrarError_fecha1(); ?>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero1" <?php $validador->mostrarNumero1(); ?>>
+        <input type="number" class="form-control" name="numero1" <?php $validador->mostrarNumero1(); ?> required>
         <?php $validador->mostrarError_numero1(); ?>
     </div>
 </div>
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 2</label>
-        <input type="date" class="form-control" name="fecha2" <?php $validador->mostrarFecha2(); ?>>
+        <input type="date" class="form-control" name="fecha2" <?php $validador->mostrarFecha2(); ?> required>
         <?php $validador->mostrarError_fecha2(); ?>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero2" <?php $validador->mostrarNumero2(); ?>>
+        <input type="number" class="form-control" name="numero2" <?php $validador->mostrarNumero2(); ?> required>
         <?php $validador->mostrarError_numero2(); ?>
     </div>
 </div>
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 3</label>
-        <input type="date" class="form-control" name="fecha3" <?php $validador->mostrarFecha3(); ?>>
+        <input type="date" class="form-control" name="fecha3" <?php $validador->mostrarFecha3(); ?> required>
         <?php $validador->mostrarError_fecha3(); ?>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero3" <?php $validador->mostrarNumero3(); ?>>
+        <input type="number" class="form-control" name="numero3" <?php $validador->mostrarNumero3(); ?> required>
         <?php $validador->mostrarError_numero3(); ?>
     </div>
 </div>
 <div class="form-row">
     <div class="form-group col-md-6">
         <label>Fecha 4</label>
-        <input type="date" class="form-control" name="fecha4" <?php $validador->mostrarFecha4(); ?>>
+        <input type="date" class="form-control" name="fecha4" <?php $validador->mostrarFecha4(); ?> required>
         <?php $validador->mostrarError_fecha4(); ?>
     </div>
     <div class="form-group col-md-6">
         <label>Días a sumar</label>
-        <input type="number" class="form-control" name="numero4" <?php $validador->mostrarNumero4(); ?>>
+        <input type="number" class="form-control" name="numero4" <?php $validador->mostrarNumero4(); ?> required>
         <?php $validador->mostrarError_numero4(); ?>
     </div>
 </div>
